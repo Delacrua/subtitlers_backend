062d048336e8ff52abe8608ad58169744a39aa2c
Uladzimir Fiodarau <ufiodarau@gmail.com>
2023-04-16 21:36:46 +0400

    update CSRF_TRUSTED_ORIGINS

--- src/app/conf/http.py
+++ src/app/conf/http.py
@@ -1,19 +1,10 @@
 from app.conf.environ import env
 
 ALLOWED_HOSTS = ["*"]  # host validation is not necessary in 2020
-CSRF_TRUSTED_ORIGINS: list = []
-
-CORS_ORIGIN_REGEX_WHITELIST = [
-    r".*localhost.*",
-    r".*127.0.0.1.*",
-    r".*192.168.*",
-    # add other origins
+CSRF_TRUSTED_ORIGINS: list = [
+    "https://*.flerman.com",
 ]
 
-if env("DEBUG"):
-    ABSOLUTE_HOST = "http://localhost:3000"
-else:
-    ABSOLUTE_HOST = "https://your.app.com"
 
 if env("DEBUG", cast=bool, default=False):
     import socket  # only if you haven't already imported this
