59ae19663ae32221da000418eed76fb9055475c4
Delacrua <ufiodarau@gmail.com>
2023-05-01 20:46:32 +0400

    allowed all cors origins to test

--- src/app/conf/http.py
+++ src/app/conf/http.py
@@ -5,7 +5,12 @@ CSRF_TRUSTED_ORIGINS: list = [
     "https://*.flerman.com",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_REGEX_WHITELIST = [
+    r".*localhost.*",
+    r".*127.0.0.1.*",
+    r".*192.168.*",
+    r"*.*netlify.*",
+]
 
 if env("DEBUG", cast=bool, default=False):
     import socket  # only if you haven't already imported this
