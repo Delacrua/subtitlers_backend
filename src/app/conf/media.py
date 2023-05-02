ce90831fa9434d920bd069ebeefa5532f56f69c7
Uladzimir Fiodarau <95965367+Delacrua@users.noreply.github.com>
2023-05-02 12:18:21 +0400

    Update media.py

--- src/app/conf/media.py
+++ src/app/conf/media.py
@@ -1,4 +1,5 @@
+from app.conf.boilerplate import BASE_DIR
 from app.conf.environ import env
 
 MEDIA_URL = "/static/media/"
-MEDIA_ROOT = env("MEDIA_ROOT", cast=str, default="vol/web/media")
+MEDIA_ROOT = BASE_DIR / env("MEDIA_ROOT", cast=str, default="vol/web/media")
