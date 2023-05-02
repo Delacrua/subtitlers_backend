0743239307007b1c16fe3416c8d6e64dfe8322e5
Uladzimir Fiodarau <95965367+Delacrua@users.noreply.github.com>
2023-05-02 15:01:45 +0400

    Update serializers.py

--- src/subtitles/api/serializers.py
+++ src/subtitles/api/serializers.py
@@ -103,10 +103,12 @@ class FilmSerializer(serializers.ModelSerializer):
 
     def get_words(self, film):  # type: ignore
         return WordSerializer(film.words.all(), many=True, context={"film_instance": film}).data
-
+    
     def get_poster(self, film):  # type: ignore
-        url_string = "%s%s" % (Site.objects.get_current().domain, film.poster.url)
-        return "https://" + url_string.replace("//", "/")
+        if film.poster:
+            url_string = "%s%s" % (Site.objects.get_current().domain, film.poster.url)
+            return "https://" + url_string.replace("//", "/")
+        return None
 
     class Meta:
         model = sbt_models.Film
